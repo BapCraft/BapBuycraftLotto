254663b4a685b81ec28625717f0a04a5ae18f140
treyzania <treyzania@gmail.com>
2015-07-22 19:46:47 -0400

    Made tickets needed and ticket price configurable via a file.

--- src/main/java/com/bapcraft/bclotto/BCLotto.java
+++ src/main/java/com/bapcraft/bclotto/BCLotto.java
@@ -1,7 +1,9 @@
 package com.bapcraft.bclotto;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.bapcraft.bclotto.commands.CommandAddTicket;
@@ -16,14 +18,19 @@ public class BCLotto extends JavaPlugin {
 	
 	public Drawing activeDrawing = null;
 	public ArrayList<Drawing> previousDrawings = new ArrayList<Drawing>();
+	public FileConfiguration config = null;
 	
 	@Override
 	public void onEnable() {
 		
 		instance = this;
+		config = this.getConfig();
 		
 		this.getCommand(NAME_ADD_TICKET_COMMAND).setExecutor(new CommandAddTicket());
 		this.getCommand(NAME_CHECK_POT_COMMAND).setExecutor(new CommandCheckPot());
+		
+		Drawing.NEEDED_TICKETS = config.getInt("tickets_needed", Drawing.NEEDED_TICKETS);
+		Ticket.TICKET_PRICE = new BigDecimal(config.getDouble("asthetic_ticket_price", Ticket.TICKET_PRICE.doubleValue())); // Yucky.
 		
 	}
 	
@@ -31,6 +38,10 @@ public class BCLotto extends JavaPlugin {
 	public void onDisable() {
 		
 		instance = null;
+		config = null;
+		
+		this.getCommand(NAME_ADD_TICKET_COMMAND).setExecutor(null);
+		this.getCommand(NAME_CHECK_POT_COMMAND).setExecutor(null);
 		
 	}
 	
