c14ed7e0e838ac6678993a408f798ecbfc434bb6
treyzania <treyzania@gmail.com>
2015-07-22 00:30:52 -0400

    Made a prize class, only notifies an admin or other person that can handle it, of a win.

---
+++ src/main/java/com/bapcraft/bclotto/prizes/PrizeNotifyAdmin.java
@@ -0,0 +1,14 @@
+package com.bapcraft.bclotto.prizes;
+
+import java.util.UUID;
+
+import com.bapcraft.bclotto.Drawing;
+
+public class PrizeNotifyAdmin extends Prize {
+
+	@Override
+	public void onWin(Drawing pot, UUID winner) {
+		// TODO Notify a user.
+	}
+
+}
