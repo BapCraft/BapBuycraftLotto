dcc5710e762946f80e459323257aa01562078c41
treyzania <treyzania@gmail.com>
2015-08-29 12:25:32 -0400

    Removed an unused import.

--- src/main/java/com/bapcraft/bclotto/commands/CommandAddTicket.java
+++ src/main/java/com/bapcraft/bclotto/commands/CommandAddTicket.java
@@ -7,8 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 import com.bapcraft.bclotto.BCLotto;
 import com.bapcraft.bclotto.Drawing;
 import com.bapcraft.bclotto.Ticket;
